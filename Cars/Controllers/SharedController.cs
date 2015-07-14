5506c98115f7dba83f1f9d1307746c8ba6f7c4ae
raulcantu <raul.cantu@definityfirst.com>
2015-07-14 17:48:29 -0500

    first commit mvc 4 project .NET

---
+++ Cars/Controllers/SharedController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cars.Controllers
+{
+    public class SharedController : Controller
+    {
+        //
+        // GET: /Shared/
+
+        #region Constructor
+        public SharedController(): base()
+        {
+        }
+        #endregion
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _InitScript(string initString)
+        {
+            var _initString = new List<string>();
+            _initString.Add(initString);
+            return PartialView(_initString);
+        }
+    }
+}
